c17e3c384fcb2b9b9189638a672b906bd2258dd2
Timur Burganov <timk90@mail.ru>
2019-09-01 22:46:29 +0300

    changes in branch 1

--- src/BranchingMain.java
+++ src/BranchingMain.java
@@ -1,5 +1,7 @@
 public class BranchingMain {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("World! From Branch #1");
+        System.out.println("It has been slightly changed...");
     }
 }
