07bc66d771509734b9854312282df3c51a398f34
Timur Burganov <timk90@mail.ru>
2019-09-01 22:54:42 +0300

    new classes added

--- src/BranchingMain.java
+++ src/BranchingMain.java
@@ -2,5 +2,6 @@ public class BranchingMain {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("World...from the second branch...");
+        System.out.println(new SecondClass("Name"));
     }
 }
---
+++ src/SecondClass.java
@@ -0,0 +1,13 @@
+public class SecondClass {
+
+    private final String name;
+
+    public SecondClass(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+}
