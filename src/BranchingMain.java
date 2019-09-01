3a4e31c03a6bc6a5e22a57dc2d9ff411df9e4a8b
Timur Burganov <timk90@mail.ru>
2019-09-01 22:51:05 +0300

    initial commit from second branch

--- src/BranchingMain.java
+++ src/BranchingMain.java
@@ -1,5 +1,6 @@
 public class BranchingMain {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("World...from the second branch...");
     }
 }
